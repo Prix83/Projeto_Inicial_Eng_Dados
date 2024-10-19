aad06230d92938b0199ecccbd914f844b8d6d911
Priscilla <prix.valenca@gmail.com>
2024-10-19 13:11:12 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Jornada de dados")
+
+#variaveis
+idade = 34
+nome = "pri"
+
+print(idade)
+
+nova_idade = idade + 1
+print(nova_idade)
+
+lista = [1,2,3,4,5,6,7]
+
+lista_invertida = lista.reverse()
+print(lista_invertida)
+
